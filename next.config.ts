1508ef5221d98d5f55ae4d5d6c7ff28e1460be8a
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2026-07-06 08:37:17 +0600

    fix: add pythonanywhere to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+ 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'devasif.pythonanywhere.com',
+      },
+    ],
+  },
+}
+
 
 export default nextConfig;
